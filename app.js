20341667c3452e51aa6580268f08f3087871dcf4
Yusuf Gümüşsoy <yusufgumussoy@Yusuf-MacBook-Air.local>
2023-12-03 11:07:29 +0300

    lesson 01

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express  from 'express'
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send ("INDEX SAYFASI")
+app.get('/', (req, res) => {
+    res.send('INDEX SAYFASI');
 });
 
-app.listen(port, ()=> {
- console.log(`Application running on port: ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Application running on port: ${port}`);
+});
